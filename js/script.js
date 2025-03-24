7b674543e3612da862dd27abe0be449047f9fac0
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-03-24 17:27:04 +0000

    taxes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates wage.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateWage() {
+  // input
+  const hoursWorked = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyWage = parseFloat(document.getElementById("hourly-wage").value)
+  const INCOME_TAX = 0.18
+
+  // process
+  const taxedSalary = (hoursWorked * hourlyWage) * (1.00 - INCOME_TAX)
+  const taxDeduction = (hoursWorked * hourlyWage) * INCOME_TAX
+
+  // output
+  document.getElementById('answer').innerHTML = 
+    'Your pay will be: $' + taxedSalary.toFixed(2) + "."
+  document.getElementById('taxes').innerHTML = 
+    'the government will take: $' + taxDeduction.toFixed(2) + "."
 }
